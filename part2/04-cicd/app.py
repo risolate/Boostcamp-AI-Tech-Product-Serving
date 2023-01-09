d6104f171317b6f97c3938c876a317b76d9ee567
risolate <99873921+risolate@users.noreply.github.com>
2023-01-09 18:56:09 +0900

    Update app.py

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - Github Action Deploy 4")
+    st.title("Mask Classification Model - Github Action Deploy 5")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
