201ae8e0452c11be5318767f5b38263582e87ffa
risolate <99873921+risolate@users.noreply.github.com>
2023-01-26 23:10:33 +0900

    Update app.py

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - Github Docker Deploy T4072")
+    st.title("Mask Classification Model - Github Docker Deploy T4072 second")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
